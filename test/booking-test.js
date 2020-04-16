6a0a2cd6e9a7164b40847f7d528c6d2b953849d6
Nick Taylor <nickstaylor@gmail.com>
2020-04-16 16:29:05 -0600

    wireframe set up for booking-test

---
+++ test/booking-test.js
@@ -0,0 +1,30 @@
+import { expect } from 'chai';
+const chai = require("chai");
+import Booking from '../src/Booking';
+import domUpdates from '../src/domUpdates';
+
+const spies = require('chai-spies');
+chai.use(spies);
+
+describe('Booking', function()){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
