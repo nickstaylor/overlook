d8bde5361eb0f5458c7888f909b9b9806bed3886
Nick Taylor <nickstaylor@gmail.com>
2020-04-16 16:31:57 -0600

    set up User class

---
+++ src/User.js
@@ -0,0 +1,23 @@
+
+class User {
+  constructor(user, bookingsData){
+    this.id = user.id;
+    this.name = user.name;
+    
+    // this.myBookings = this.findMyBookings(user, bookingsData)
+
+  }
+
+  findMyBookings(user, bookingsData) {
+ //gets previous bookings
+  }
+
+  createBooking(){
+  }
+
+
+
+}
+
+
+export default User
