5b9ec940c798a721e94a95ba8a48301c6e18568e
Nick Taylor <nickstaylor@gmail.com>
2020-04-16 16:32:12 -0600

    set up Manager class

---
+++ src/Manager.js
@@ -0,0 +1,35 @@
+// import domUpdates from '.domUpdates'
+import User from './User'
+import Hotel from './Hotel'
+
+class Manager {
+  constructor(person){
+  //   super()
+    this.id = person
+    // this.bookings = allBookings
+    // this.todaysBookings = findTodaysBookings()
+    // this.addRoomsToBookings(allRooms)
+
+  }
+
+
+  addRoomsToBookings(){
+    //adding the booking information to the bookings
+  }
+
+  createBooking(){
+
+  }
+
+  deleteBooking(){
+
+  }
+
+  findUser(){
+    //drop down os users.
+  }
+
+}
+
+
+export default Manager
