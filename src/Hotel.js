a2167c56d11e1ea899484a52744f4d1d5df45a39
Nick Taylor <nickstaylor@gmail.com>
2020-04-19 08:23:42 -0600

    add check rooms available method to hotel

--- src/Hotel.js
+++ src/Hotel.js
@@ -1,24 +1,57 @@
+import moment from 'moment';
+
 class Hotel {
   constructor(){
     this.allRooms = []
+    this.allBookings = []
+    this.todaysBookings = []
+    this.todaysDate = moment().format("YYYY/MM/DD")
 
   }
-
-  findUser(){
-    // find specific user and display using domUpdates
-  }
+  // this.todaysBookings = findTodaysBookings()
 
   calculateRevenue(){
     //caclulate revenue
   }
 
-  roomsAvailToday(){
-
+  filterRoomsByType(){
+   //move to manager
   }
 
+  checkRoomsAvailable(date){
+    //for today or any date
+  }
+
+  checkUserBookings(id){
+    return this.allBookings.filter(booking => booking.userID === id)
+
+
+    }
+
+
+  // generateFullIngredientList(partialIngredients) {
+  //       return partialIngredients.map(ingredient => {
+  //
+  //       let matchedIngredient = rawIngredientsDataPantry.find(rawIngredient =>{
+  //           return ingredient.ingredient === rawIngredient.id
+  //         })
+  //
+  //         return {name:matchedIngredient.name,
+  //                 id: matchedIngredient.id,
+  //                 estimatedCostInCents: matchedIngredient.estimatedCostInCents,
+  //                 amount: ingredient.amount
+  //               };
+  //     })
+  //   }
 
 
 
+
+//manager can use this and input any ID they want.
+
+  caclulateUserCost(id){
+
+  }
 
 }
 
